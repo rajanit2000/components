7317d74ea7601bb45da91e53dce15ba4538c8e5a
Rajan <rajanit2000@gmail.com>
2016-09-23 12:48:38 +0530

    Add files via upload

---
+++ paypal.php
@@ -0,0 +1,26 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="rajan@a2ztechnologies.in">
+<input type="hidden" name="item_name"
+value="Memorex 256MB Memory Stick">
+<input type="hidden" name="item_number" value="MEM32507725">
+<input type="hidden" name="amount" value="3">
+<input type="hidden" name="tax" value="1">
+<input type="hidden" name="quantity" value="1">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="USD">
+
+<!-- Enable override of buyers's address stored with PayPal . -->
+<input type="hidden" name="address_override" value="1">
+<!-- Set variables that override the address stored with PayPal. -->
+<input type="hidden" name="first_name" value="John">
+<input type="hidden" name="last_name" value="Doe">
+<input type="hidden" name="address1" value="345 Lark Ave">
+<input type="hidden" name="city" value="San Jose">
+<input type="hidden" name="state" value="CA">
+<input type="hidden" name="zip" value="95121">
+<input type="hidden" name="country" value="US">
+<input type="image" name="submit" border="0"
+src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+alt="PayPal - The safer, easier way to pay online">
+</form>
